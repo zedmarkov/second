f30cab97f88a1676a191f9b4ed50bad98abf640a
zed <zedmarkov@gmail.com>
2020-04-29 23:46:04 +0300

    29/04/2020

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(done) {
+    gulp.src("scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
+
+
+    done();
+});
+
+gulp.task('start', function(done) {
+
+    browserSync.init({
+        server: ""
+    });
+
+    gulp.watch("scss/*.scss", gulp.series('sass'));
+    gulp.watch("*.html").on('change', () => {
+      browserSync.reload();
+      done();
+    });
+
+
+    done();
+});
+
+gulp.task('default', gulp.series('sass', 'start'));
